5329137506dde211c702fd6114fefe05208e48d3
nishan4477 <104998929+nishan4477@users.noreply.github.com>
2023-06-27 19:42:58 +0545

    scrolltotop added

--- Pages/ContactUs.jsx
+++ Pages/ContactUs.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const ContactUs = () => {
+  const a= (()=>{
+    alert("Your message has been submitted")
+  })
+ 
   return (
     <div data-aos="fade-up"  className='contact '>
                 <div  className="contactbanner d-flex justify-content-center align-items-center ">
@@ -11,10 +15,11 @@ const ContactUs = () => {
         </div>
         
         <div className="container py-4 text-center">
-            <h3 data-aos="fade-right" className='fw-1 text-dark'>Send a message to us!</h3>
+            <h3 data-aos="fade-right" className='fw-1 text-dark'>Send a message to us!</h3> 
             <form className='border rounded shadow py-4'>
               <label for="name">FullName:</label>
-                <input className='px-5 py-2 my-4 ' type='text' name='name' placeholder='Enter your FullName here ' autoComplete='off' required></input><br /><hr />
+                <input className='px-5 py-2 my-4 ' type='text' name='name' placeholder='Enter your FullName here ' autoComplete='off' required></input><br />
+                <hr />
                 <label for="location">Location:</label>
                 <input className='px-5 py-2 my-4 ' type='text' name='location' placeholder='Enter your Location here ' autoComplete='off' required></input><br /><hr />
                 <label for="mail">Email:</label>
@@ -22,9 +27,9 @@ const ContactUs = () => {
                 <label for="subject">Subject:</label>
                 <input className='px-5 py-2 my-4 ' type='text' name='subject' placeholder='Subject '></input><br /><hr />
                 <textarea className='px-5 py-2 my-4 ' rows="5" cols="40" name='msg' placeholder='Enter your message' ></textarea><br />
-                <button type='submit'  className='btn btn-dark px-5 '>Submit</button>
-                
-               
+                <button className='btn btn-dark ' id="contactSubmit" type="submit" onClick={a}>Submit</button>
+
+  
                 
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./assets/style.scss"
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from '../Component/ScrollToTop.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><BrowserRouter>  <App /></BrowserRouter>
+  <React.StrictMode><BrowserRouter> <ScrollToTop/> <App /></BrowserRouter>
   
   </React.StrictMode>,
 )
